96a84ad040c4a43a3f443f562d66271ea76f09bb
appfogtest8 <appfogtest8@mm.st>
2013-11-24 19:48:50 -0800

    init commit

---
+++ fizzbuzz.lua
@@ -0,0 +1,14 @@
+print('integer? ')
+eulogy = io.read()
+
+for i = 1, eulogy do
+	if i % 15 == 0 then
+		print('fizzbuzz')
+	elseif i % 3 == 0 then
+		print('fizz')
+	elseif i % 5 == 0 then
+		print('buzz')
+	else
+		print(i)
+	end
+end
